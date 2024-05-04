f0cae34698ae9c485884aaa79466d3ed6b7f51ad
creative-site <163428887+creative-site@users.noreply.github.com>
2024-05-04 20:26:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+$(function () {
+    $(".openbtn1").click(function () {//ボタンがクリックされたら
+        $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+        $("#navi").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    });
 
+    $("#navi a").click(function () {//ナビゲーションのリンクがクリックされたら
+        $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+        $(".navi").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+
+    });
+});
